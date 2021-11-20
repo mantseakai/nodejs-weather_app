9e8ef3d5504f8cc03f0ae49d61e6a17f716ea10a
Mantse Akai <mantseakai@gmail.com>
2021-11-20 08:55:12 +0000

    Adding Additional Information

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,7 @@ const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 const msg_forecast = document.querySelector('#forecast')
 const msg_error = document.querySelector('#error')
+const msg_another = document.querySelector('#msg_other')
 
 
 msg_forecast.innerHTML = ''
@@ -32,6 +33,7 @@ weatherform.addEventListener('submit', (e)=>{
 
 				msg_forecast.innerHTML = data.location
 				msg_error.innerHTML = data.forecast.description
+				msg_another.innerHTML = data.forecast.temperature +' degrees but feels like '+ data.forecast.feelslike
 			}
 			
 		})
